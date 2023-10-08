f57d24d1f8d60a58803b2700156aebccb72f3bdb
loyarabhishek92 <147045461+loyarabhishek92@users.noreply.github.com>
2023-10-08 16:35:15 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let typed = new Typed(".multiple-text", {
+    strings: ["IT Student at NMSS", "Frontend Developer", "YouTuber", "Blogger", "Tiktoker"],
+    Typespeed:8000000,
+    Backspeed:8000000,
+    loop:true
+})
